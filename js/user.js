6c3461b86bd6b90419639b9190a3aab77f2ecf9c
Giuliana <giuliana.torres05@gmail.com>
2022-07-25 19:37:55 -0600

    Added hidePageComponents() to updateUIOnUserLogin().

--- js/user.js
+++ js/user.js
@@ -106,6 +106,8 @@ function saveUserCredentialsInLocalStorage() {
 function updateUIOnUserLogin() {
   console.debug("updateUIOnUserLogin");
 
+  hidePageComponents();
+
   putStoriesOnPage();
   $allStoriesList.show();
 
